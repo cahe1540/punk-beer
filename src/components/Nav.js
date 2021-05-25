a48fddc8899f125fefd6ad9d7abd6d33aec67bcd
Carlos Herrera <cahe1540@gmail.com>
2021-05-25 09:11:38 -0700

    fixed styling for smaller screens

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export const Nav = () => {
     return (
         <section className = 'nav'>
-             <h3 className = 'title'><Link href='/'>Beans Love Beer</Link></h3>
+             <h3 className = 'title'><Link to='/'>Beans Love Beer</Link></h3>
              <ul className = 'nav-menu'>
-                <li><Link to = '/'>Home</Link></li>
-                <li><Link to = '/favorites'>Favorites</Link></li> 
+                <li><Link to= "/">Home</Link></li>
+                <li><Link to= "/favorites">Favorites</Link></li> 
              </ul>
         </section>
     )
